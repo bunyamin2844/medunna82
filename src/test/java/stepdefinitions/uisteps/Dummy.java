42869dc4b92f9aecbeaa7543455bb9356dd6b6f7
Bunyamin <bunyaminbektas2844@gmail.com>
2022-03-05 23:17:02 +0000

    dummy

--- src/test/java/stepdefinitions/uisteps/Dummy.java
+++ src/test/java/stepdefinitions/uisteps/Dummy.java
@@ -5,9 +5,9 @@ import com.github.javafaker.Faker;
 public class Dummy {
 
     public static void main(String[] args) {
-        String name = "Ibrahim";
-
-        Faker  faker = new Faker();
+        System.out.println("****************");
+        int a = 4;
+        //Faker  faker = new Faker();
         //name = faker.name().firstName();
         System.out.println(name);
 
