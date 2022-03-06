0be1dc6633f0581b8efe2afedbbeea4ea0670d02
ibrahim aygun <ssibrahimaygun@gmail.com>
2022-03-06 15:09:28 +0300

    commit first

--- src/test/java/pages/Appointment.java
+++ src/test/java/pages/Appointment.java
@@ -2,6 +2,6 @@ package pages;
 
 public class Appointment {
     public static void main(String[] args) {
-        System.out.println("Emir Cakir drago");
+        System.out.println("ibrahim aygun drago");
     }
 }
