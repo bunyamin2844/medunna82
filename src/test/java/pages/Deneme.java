f55afe05dc0d3048c7127583b0d91b5cd91bda15
RabiaAkgun <rabiaakgun28@gmail.com>
2022-03-12 01:00:48 +0300

    git deneme

--- src/test/java/pages/Deneme.java
+++ src/test/java/pages/Deneme.java
@@ -1,11 +1,15 @@
 package pages;
 
 public class Deneme {
-    int a = 5;
-    int b = 7;
-    double c= 3.2;
-    boolean x = true;
-    boolean y = false;
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        double c= 3.2;
+        boolean x = true;
+        boolean y = false;
+        System.out.println(a+b);
+    }
+
 
 
 
