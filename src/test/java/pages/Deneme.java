09226073d31a6c9c4847cf642f6b21eddd2da0fa
salihsanliturk <sanliturksalih@gmail.com>
2022-03-11 23:34:48 +0200

    git deneme

---
+++ src/test/java/pages/Deneme.java
@@ -0,0 +1,22 @@
+package pages;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Deneme {
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+    int a = 10;
+    int b = 7;
+    double c = 3.7;
+    boolean x = false;
+
+
+}
