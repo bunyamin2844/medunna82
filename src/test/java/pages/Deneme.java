f5e5ba414a440028061177a88a4acb1aecf805ec
Bunyamin <bunyaminbektas2844@gmail.com>
2022-03-11 21:22:03 +0000

    git

---
+++ src/test/java/pages/Deneme.java
@@ -0,0 +1,13 @@
+package pages;
+
+public class Deneme {
+    public static void main(String[] args) {
+        System.out.println("deneme" +
+                "");
+        git denme
+        git denme
+        git denme
+        git denme
+
+    }
+}
