3dbd8dd48b57b86e61bb315e7e03c3857db9d5b9
ibrahim aygun <ssibrahimaygun@gmail.com>
2022-03-26 12:01:41 +0300

    commit last

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -7,37 +8,74 @@ import utilities.Driver;
 
 public class RegistrationPage {
 
+
     public RegistrationPage(){
-        PageFactory.initElements(Driver.getDriver(), this);
+
+
+        PageFactory.initElements(Driver.getDriver(),this);
     }
 
+    @FindBy(id = "ssn")
+    public WebElement ssnTextbox;
 
-    @FindBy(id="ssn")
-    public WebElement ssnTextBox;
     @FindBy(id = "firstName")
-    public WebElement firstnameTextBox;
+    public WebElement firstnameTextbox;
+
     @FindBy(id = "lastName")
-    public WebElement lastnameTextBox;
-    @FindBy(id = "username")
-    public WebElement usernameTextBox;
-    @FindBy(id = "email")
-    public WebElement emailTextbox;
-    @FindBy(xpath = "//*[text()='New password']/following::input")
-    public WebElement firstPasswordTextBox;
-    @FindBy(id= "secondPassword")
-    public WebElement newPasswordTextBox;
+    public WebElement lastnameTextbox;
+
+    @FindBy(name = "username")
+
+    public WebElement usernameTextbox;
+
+    @FindBy(id="email")
+
+    public WebElement emaiTextbox;
+
+    @FindBy(id = "firstPassword")
+
+    public WebElement passwordTextbox;
+
+    @FindBy(id = "secondPassword")
+
+    public WebElement passwordConfirmationTextbox;
+
     @FindBy(id = "register-submit")
+
     public WebElement registerButton;
-    @FindBy(xpath = "//*[contains(text(),'Registration Saved')]")
+
+    @FindBy(xpath = "//*[text()='Registration Saved']")
     public WebElement successMessageToastContainer;
+
+    //Mr Necip
 
     @FindBy(xpath = "//*[@id='strengthBar']/li[contains(@style,'rgb(255, 0, 0)')]")
     public WebElement passwordStrength1;
-
     @FindBy(xpath = "//*[@id='strengthBar']/li[contains(@style,'rgb(255, 153, 0)')]")
     public WebElement passwordStrength2;
-
     @FindBy(xpath = "//*[@id='strengthBar']/li[contains(@style,'rgb(153, 255, 0)')]")
     public WebElement passwordStrength3;
+
+    @FindBy(xpath = "//div[contains(text(),'Your SSN is invalid')]")
+    public WebElement ssnInvalidMessage;
+    @FindBy(xpath = "//div[contains(text(),'Your SSN is required.')]")
+    public WebElement ssnRequiredMessage;
+    @FindBy(xpath = "//div[contains(text(),'Your FirstName is required.')]")
+    public WebElement firstNameRequiredMessage;
+    @FindBy(xpath = "//div[contains(text(),'Your LastName is required.')]")
+    public WebElement lastNameRequiredMessage;
+
+
+    //Mrs Zehra
+
+    public WebElement returnPasswordStrength(String rgb){
+        return Driver.getDriver().findElement(By.xpath("//*[@id='strengthBar']/li[contains(@style,'"+rgb+"')]"));
+    }
+
+
+
+     @FindBy(xpath = "//*[@id='strengthBar']/li[contains(@style,'rgb(153, 255, 0)')]")
+    public WebElement passwordStrength4;
+
 
 }
